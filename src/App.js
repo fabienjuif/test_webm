bee333c4a96e277c0aaa04c8ce7ec79cc021f2ca
Fabien JUIF <fabien.juif@gmail.com>
2020-08-28 18:51:57 +0200

    webm

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState, useCallback } from "react";
+import video from "./test.webm";
+import "./App.css";
 
 function App() {
+  const videoRef = useRef();
+  const [color, setColor] = useState();
+
+  const onClick = useCallback(() => {
+    setColor((old) => {
+      switch (old) {
+        case "red":
+          return "green";
+        case "green":
+          return "white";
+        default:
+          return "red";
+      }
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ backgroundColor: color }}>
+      <video
+        ref={videoRef}
+        src={video}
+        loop
+        onClick={onClick}
+        preload="auto"
+        muted
+        width={300}
+        style={{
+          border: "1px solid black",
+        }}
+        onMouseEnter={() => {
+          videoRef.current.play();
+        }}
+        onMouseLeave={async () => {
+          // play is a promise so wait play before pause cf: https://developers.google.com/web/updates/2017/06/play-request-was-interrupted
+          await videoRef.current.play();
+          videoRef.current.currentTime = 0;
+          await videoRef.current.pause();
+        }}
+      />
     </div>
   );
 }
